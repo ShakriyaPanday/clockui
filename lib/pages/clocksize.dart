cac718507ed56dd1ee27f78d2f3dbb7ce4618f89
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-14 23:40:52 +0545

    Window changing button added

--- lib/pages/clocksize.dart
+++ lib/pages/clocksize.dart
@@ -1,21 +1,84 @@
 import 'package:clockui/pages/spotify.dart';
+import 'package:clockui/pages/translate.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class ClockDesign extends StatelessWidget {
+class ClockDesign extends StatefulWidget {
   const ClockDesign({super.key});
 
   @override
+  State<ClockDesign> createState() => _ClockDesignState();
+}
+
+class _ClockDesignState extends State<ClockDesign> {
+  Widget clockWidget = SpotifyUi();
+  @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      padding: EdgeInsets.all(15),
-      height: 220,
-      width: 220,
-      decoration: BoxDecoration(
-        color: Colors.grey[900],
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: SpotifyUi(),
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: EdgeInsets.all(15),
+          height: 220,
+          width: 220,
+          decoration: BoxDecoration(
+            color: Colors.grey[900],
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: clockWidget,
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  clockWidget = SpotifyUi();
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.green[600]),
+                child: Text(
+                  "Spotify",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  clockWidget = TranslateUi();
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue[600]),
+                child: Text(
+                  "Translate",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
     ));
   }
 }
---
+++ lib/pages/translate.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TranslateUi extends StatelessWidget {
+  const TranslateUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("translate"),
+    );
+  }
+}
