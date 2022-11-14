074ec2ae71428a245da5940239f8b6d75dc90d3d
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-14 22:14:26 +0545

    first commit

---
+++ lib/pages/clocksize.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClockDesign extends StatelessWidget {
+  const ClockDesign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(color: Colors.black),
+    ));
+  }
+}
