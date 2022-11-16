f034ea46c795ea31de5f7d6e719eeb037a8ec696
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-16 22:17:54 +0545

    Google translate photo added

--- lib/pages/translate.dart
+++ lib/pages/translate.dart
@@ -7,46 +7,53 @@ class TranslateUi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 110,
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey[800],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "English",
-                      style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17),
-                    ),
-                    Icon(
-                      CupertinoIcons.arrow_down,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "Nepali",
-                      style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 17),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
-      ),
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 110,
+              height: 100,
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "English",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17),
+                  ),
+                  Icon(
+                    CupertinoIcons.arrow_down,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "नेपाली",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: Image.asset("image/google.png"),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
