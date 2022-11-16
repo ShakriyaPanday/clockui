0b4427e871cb44e391163bc4d6175e4382056219
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-16 22:09:47 +0545

    translate text added

--- lib/pages/translate.dart
+++ lib/pages/translate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TranslateUi extends StatelessWidget {
   const TranslateUi({super.key});
@@ -12,11 +13,33 @@ class TranslateUi extends StatelessWidget {
           Row(
             children: [
               Container(
+                width: 110,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("English"),
-                    Icon(CupertinoIcons.arrow_down),
-                    Text("Nepali")
+                    Text(
+                      "English",
+                      style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    ),
+                    Icon(
+                      CupertinoIcons.arrow_down,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "Nepali",
+                      style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17),
+                    ),
                   ],
                 ),
               )
